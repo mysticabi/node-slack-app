4db95603679b276a7e814de0eeead16453fd97d6
mysticabi <mystic.abi1@gmail.com>
2021-10-02 08:36:05 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { App } = require('@slack/bolt');
 
-const slackSigningSecret = '4f5bc0a746b803da2eedc471ecd45d5c';
-const slackAccessToken = 'xoxb-2491257843379-2566898424480-JsWKRu6FtUrXkcW2lOMboCfL';
+const slackSigningSecret = '';
+const slackAccessToken = '';
 
 
 // Initializes your app with your bot token and signing secret
@@ -10,57 +10,13 @@ const app = new App({
   signingSecret: slackSigningSecret
 });
 
+
 // Listens to incoming messages that contain "hello"
-app.message('estore', async ({ message, say }) => {
+app.command('/estore', async ({ command, ack, respond }) => {
   // say() sends a message to the channel where the event was triggered
-  await say({
-    blocks: [
-      {
-			"type": "header",
-			"text": {
-				"type": "plain_text",
-				"text": "Welcome to eSwag! Choose the options below"
-			}
-		},
-		{
-			"type": "actions",
-			"elements": [
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "List Products",
-						"emoji": true
-					},
-					"style": "primary",
-					"value": "list_products",
-					"action_id": "list_products"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "Review Cart",
-						"emoji": true
-					},
-					"value": "review_cart",
-					"action_id": "review_cart"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "Checkout",
-						"emoji": true
-					},
-					"value": "checkout",
-					"action_id": "checkout"
-				}
-			]
-		}
-    ],
-    text: `Hey there <@${message.user}>!`
-  });
+
+  await ack();
+  await respond('test this');
 });
 
 app.action('list_products', async ({ body, ack, say }) => {
